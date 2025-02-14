15c8eacd184d2406a8bc05a982b6a1d8e0c5071e
Pankaj Gupta <pgupta584@gmail.com>
2025-02-14 20:01:49 +0530

    first commit

---
+++ src/main/java/selenium/MyFirstTest.java
@@ -0,0 +1,34 @@
+package selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MyFirstTest {
+    public static void main(String[] args) {
+        // Set the path to the ChromeDriver executable
+        System.setProperty("webdriver.chrome.driver", "/Users/payzapp-automation/Documents/Practice-Demo/MSchool/Java-Selenium/drivers/chromedriver"); // Update the path
+
+        // Create a new instance of ChromeDriver (or any other driver)
+        ChromeDriver driver = new ChromeDriver();
+
+        // Open a website
+        driver.get("https://www.facebook.com/");
+
+        // Print the page title
+        System.out.println("Page Title: " + driver.getTitle());
+        //WebElement email_field = driver.findElement(By.id("email"));
+        //WebElement email_field = driver.findElement(By.name("email"));
+        //WebElement email_field = driver.findElement(By.tagName("input"));
+//        WebElement email_field = driver.findElement(By.className("inputtext _55r1 _6luy"));
+//        WebElement link = driver.findElement(By.linkText("Forgotten password?"));
+//        WebElement link = driver.findElement(By.linkText("Forgotten password?"));
+//        link.click();
+//        WebElement email_field = driver.findElement(By.cssSelector("input[id=\"email\"]"));
+        WebElement email_field = driver.findElement(By.xpath("//input[@id='email']"));
+        email_field.sendKeys("Pankaj Gupta");
+
+        // Close the browser
+        //driver.quit();
+    }
+}
