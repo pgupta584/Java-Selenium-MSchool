5e7fe0414e9ea564500d771ce7585df02426b8a8
Pankaj Gupta <pgupta584@gmail.com>
2025-03-20 21:39:27 +0530

    POM

--- src/main/java/day16/TestRunner.java
+++ src/main/java/day16/TestRunner.java
@@ -4,7 +4,8 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = "/Java-Selenium/src/test/resources/login.feature" // Path to your feature files
+        features = "/Users/payzapp-automation/Documents/Practice-Demo/MSchool/Java-Selenium/src/test/resources/login.feature", // Path to your feature files
+        glue = "day16"
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
 }
